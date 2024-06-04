30eb7e17d7b3888348b548c4414295aadd0a3710
Jean Stéphane <ledroitalaporteedetous@gmail.com>
2024-06-04 16:39:26 +0200

    PUT

--- src/app/api/tasks/[id]/route.ts
+++ src/app/api/tasks/[id]/route.ts
@@ -1,9 +1,25 @@
 import { NextResponse } from "next/server";
+import {connectDB} from "@/utils/mongoose"
+import Task from "@/models/Task"
 
-export function GET(request, { params }) {
-    return NextResponse.json({
-        message: `obteniendo tarea ${params.id}...`,
-    });
+export async function GET(request, { params }) {
+    try {
+    connectDB()
+    const taskFound = await Task.findById(params.id)
+
+    if (!taskFound)
+        return NextResponse.json({
+        message: "Task not found",
+    }, {
+        status: 404
+    })
+
+    return NextResponse.json(taskFound);
+}   catch (error) {
+   return NextResponse.json(error.message, {
+        status: 400
+    })
+}
 }
 
 export function DELETE(request, {params}) {
@@ -12,10 +28,19 @@ export function DELETE(request, {params}) {
     })
 }
 
-export function PUT(request, { params }) {
-    return NextResponse.json({
-        message: `actualisando tarea ${params.id}...`,
-    })
+export async function PUT(request, { params }) {
+    try {
+      const data = await request.json()
+      const taskUpdated =await Task.findByIdAndUpdate(params.id, data, {
+        new: true
+      })
+      return NextResponse.json(taskUpdated)
+    } catch (error) {
+      return NextResponse.json(error.message, {
+            status: 400
+        })
+
+    }
 }
 
 
