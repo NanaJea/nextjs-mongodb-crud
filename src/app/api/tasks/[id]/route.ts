a324e8d984f7a5a48758314a839fc738fc5c7fe5
Jean Stéphane <ledroitalaporteedetous@gmail.com>
2024-06-04 12:30:31 +0200

    Route Handlers

---
+++ src/app/api/tasks/[id]/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+
+export function GET(request, { params }) {
+    return NextResponse.json({
+        message: `obteniendo tarea ${params.id}...`,
+    });
+}
+
+export function DELETE(request, {params}) {
+    return NextResponse.json({
+        message: `eliminando tarea ${params.id}...`,
+    })
+}
+
+export function PUT(request, { params }) {
+    return NextResponse.json({
+        message: `actualisando tarea ${params.id}...`,
+    })
+}
+
+
---
+++ src/app/api/tasks/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+
+export function GET() {
+    return NextResponse.json({
+        message: "obteniendo tareas...",
+    }); 
+}
+
+export function POST() {
+    return NextResponse.json({
+        message: "creando tarea...",
+    })
+}
